3e4cde651d2630ee16c7c24fdb30ce12357f7ffb
FraLeone837 <10572353@polimi.it>
2020-07-03 21:00:38 +0200

    adds all deliveries

--- deliveries/code/src/main/java/Model/ForbiddenConstruction.java
+++
@@ -1,7 +0,0 @@
-package Model;
-
-public class ForbiddenConstruction extends Invisible {
-    public ForbiddenConstruction(Player creator){
-        super(creator);
-    }
-}
--- deliveries/code/src/main/java/View/Interfaces/ServerObserver.java
+++
@@ -1,8 +0,0 @@
-package View.Interfaces;
-
-import Controller.Communication.Message;
-
-public interface ServerObserver {
-    void didReceiveMessage(Message newMsg);
-    void didReceiveMessage(Message newMsg, int ID);
-}
